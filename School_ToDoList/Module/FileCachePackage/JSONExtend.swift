73cddd835523284936ee837f2e2b77af615248ee
WotRemboy <tverpokhdeb@icloud.com>
2023-07-19 13:42:28 +0300

    Local FileCachePackage

---
+++ School_ToDoList/Module/FileCachePackage/JSONExtend.swift
@@ -0,0 +1,59 @@
+//
+//  JSONExtend.swift
+//  School_ToDoList
+//
+//  Created by Roman Tverdokhleb on 19.07.2023.
+//
+
+import Foundation
+
+extension ToDoItem {
+    public static func parse(json: Any) -> ToDoItem? {
+        guard let data = json as? [String: Any],
+              let id = data["id"] as? String,
+              let taskText = data["taskText"] as? String,
+              let createDateInt = data["createDate"] as? Int
+        else {
+            return nil
+        }
+        
+        let createDate = Date(timeIntervalSince1970: TimeInterval(createDateInt))
+        let completed = data["completed"] as? Bool
+        let importance = (data["importance"] as? String).flatMap(Importance.init(rawValue:))
+        let deadline = (data["deadline"] as? Int).flatMap { timestamp -> Date? in
+            return Date(timeIntervalSince1970: TimeInterval(timestamp))
+        }
+        let editDate = (data["editDate"] as? Int).flatMap { timestamp -> Date? in
+            return Date(timeIntervalSince1970: TimeInterval(timestamp))
+        }
+
+        return ToDoItem(id: id,
+                        taskText: taskText,
+                        importance: importance ?? .regular,
+                        deadline: deadline,
+                        completed: completed ?? false,
+                        createDate: createDate,
+                        editDate: editDate)
+    }
+    
+    public var json: Any {
+        var dictionary: [String: Any] = [
+            "id": id,
+            "taskText": taskText,
+            "completed": completed
+        ]
+        
+        if importance != .regular {
+            dictionary["importance"] = importance.rawValue
+        }
+        if let deadline = deadline {
+            dictionary["deadline"] = Int(deadline.timeIntervalSince1970)
+        }
+        dictionary["createDate"] = Int(createDate.timeIntervalSince1970)
+        if let editDate = editDate {
+            dictionary["editDate"] = Int(editDate.timeIntervalSince1970)
+        }
+        
+        return dictionary
+    }
+}
